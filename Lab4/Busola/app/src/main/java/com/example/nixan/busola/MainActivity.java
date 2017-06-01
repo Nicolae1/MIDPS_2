6c3dda406b12615fa391528f773760311d8b3a0a
Nicolae1 <nixan355@gmail.com>
2017-06-01 23:21:16 +0300

    Lab4

--- Lab4/Busola/app/src/main/java/com/example/nixan/busola/MainActivity.java
+++ Lab4/Busola/app/src/main/java/com/example/nixan/busola/MainActivity.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private float[] mGravity = new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float azimuth = 0f;
+    private float currectAzimuth = 0f;
+    private SensorManager mSensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
