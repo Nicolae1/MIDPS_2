665fc5be61aa7d0d8981bbb92c3820240e35ca68
Nicolae1 <nixan355@gmail.com>
2017-06-01 23:30:00 +0300

    Lab4

--- Lab4/Busola/app/src/main/java/com/example/nixan/busola/MainActivity.java
+++ Lab4/Busola/app/src/main/java/com/example/nixan/busola/MainActivity.java
@@ -43,5 +43,45 @@ public class MainActivity extends AppCompatActivity {
         mSensorManager.unregisterListener(this);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        final float alpha = 0.97f;
+        synchronized (this){
+            if( sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            {
+                mGravity[0]=alpha*mGravity[0]+(1-alpha)*sensorEvent.values[0];
+                mGravity[1]=alpha*mGravity[1]+(1-alpha)*sensorEvent.values[1];
+                mGravity[2]=alpha*mGravity[2]+(1-alpha)*sensorEvent.values[2];
+            }
 
+            if( sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            {
+                mGeomagnetic[0]=alpha*mGeomagnetic[0]+(1-alpha)*sensorEvent.values[0];
+                mGeomagnetic[1]=alpha*mGeomagnetic[1]+(1-alpha)*sensorEvent.values[1];
+                mGeomagnetic[2]=alpha*mGeomagnetic[2]+(1-alpha)*sensorEvent.values[2];
+            }
+
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean succes = SensorManager.getRotationMatrix(R,I,mGravity,mGeomagnetic);
+            if (succes)
+            {
+                float orientation []=new float[3];
+                SensorManager.getOrientation(R,orientation);
+                azimuth = (float)Math.toDegrees(orientation[0]);
+                azimuth = (azimuth+360)%360;
+
+                Animation anim = new RotateAnimation(-currectAzimuth,azimuth,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                currectAzimuth = azimuth;
+
+                anim.setDuration(500);
+                anim.setRepeatCount(0);
+                anim.setFillAfter(true);
+
+                imageView.startAnimation(anim);
+            }
+
+        }
+
+    }
 }
